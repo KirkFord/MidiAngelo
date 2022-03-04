83db50f990ad5170e182da4887344bdc4a6ade65
Kirk Ford <kwf014@usask.ca>
2022-03-04 16:42:14 -0600

    seperated all of my conversion functions into commands which can be run in the command line. Added examples how to run the code with documentation.

---
+++ MidiConvert/midi_to_audio_conversion.py
@@ -0,0 +1,11 @@
+import sys
+from midi2audio import FluidSynth
+
+soundfont_name = sys.argv[1]
+midi_in_name = sys.argv[2]
+wav_output_name = sys.argv[3]
+#this section has the actual MIDI to .wav/.flac
+# within the fluidsynth param you put the name/path of the sf2 (soundfont)
+# midi_to_audio takes a midi file as the first parameter and the name of the output file as the second parameter.
+# if we use .flac the file size is much around 14x smaller than a .wav file. mp3s are not supported through this (to my knowledge)
+FluidSynth('soundfonts/' + str(soundfont_name)).midi_to_audio(midi_in_name, wav_output_name)
