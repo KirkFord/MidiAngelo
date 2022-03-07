d41dcbf3c6de55dc0c830989bfadef3057860f8d
Chris Baker <csb048@usask.ca>
2022-03-07 15:27:09 -0600

    added unpacking funtion to midiAngeloConversions.py with will take a data string and convert it into a grid of pixel values so the conversions can be ran from the command line without having to open a txt file

--- MidiConvert/canvas2midi.py
+++ MidiConvert/canvas2midi.py
@@ -6,4 +6,5 @@ if n != 3 :
     print("INVALID INPUT: Three inputs required,", n-1,"provided.\n REQUIRED PARAMITERS: inputFileName outputFileName")
     exit(1)
 
-midiAngeloConversions.image2midi(sys.argv[1],sys.argv[2])
+print("calling ")
+midiAngeloConversions.canvas2midi(sys.argv[1],sys.argv[2])
