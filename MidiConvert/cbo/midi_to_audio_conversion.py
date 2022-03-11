b5574c00bf488bfdcf76a585812a1006bc3fa378
Kirk Ford <kwf014@usask.ca>
2022-03-11 17:31:16 -0600

    test cases are all gucci now

--- MidiConvert/cbo/midi_to_audio_conversion.py
+++ MidiConvert/cbo/midi_to_audio_conversion.py
@@ -1,25 +1,32 @@
 import os
 import re
-
 from midi2audio import FluidSynth
 from pydub import AudioSegment
 
 from numpy import number
 
 """
-soundfont_name = sys.argv[1]
-midi_in_name = sys.argv[2]
-wav_output_name = sys.argv[3]
-#this section has the actual MIDI to .wav
-# within the fluidsynth param you put the name/path of the sf2 (soundfont)
-# midi_to_audio takes a midi file as the first parameter and the name of the output file as the second parameter.
-# if we use .flac the file size is much around 14x smaller than a .wav file. mp3s are not supported through this (to my knowledge)
-FluidSynth('soundfonts/' + str(soundfont_name)).midi_to_audio(midi_in_name, wav_output_name)
+createWav takes:
+a string name representing the soundbank being used for the conversion.
+a string name representing the name of the midi file we wish to use with the soundbank.
+a string name representing the name of the outputted .wav file we would like to save the conversion as.
+an integer (other than zero). Positive integers will make the .wav louder, while negative integers will make the .wav quieter.
+
+creates 1 .wav file from the midi file inputted using the inputted soundbanks voice
 """
 
-
 def createWav(midi_in_name, soundfont_name, wav_output_name, db_boost=0):
-    if db_boost != 0:
+    test_flag = -1
+    if midi_in_name[-3:] != 'mid' or midi_in_name[-4:] != 'midi':
+        return test_flag
+
+    if soundfont_name[-3:] != 'sf2':
+        return test_flag
+
+    if wav_output_name[-3:] != 'wav':
+        return test_flag
+
+    if db_boost != 0:   # raise or lower volume
         temp_wav_name = "temp_" + str(wav_output_name)
         FluidSynth(str(soundfont_name)).midi_to_audio(midi_in_name, temp_wav_name)
 
@@ -28,29 +35,55 @@ def createWav(midi_in_name, soundfont_name, wav_output_name, db_boost=0):
         if volume > 0:
             volume_song = song + volume
             volume_song.export(str(wav_output_name), format="wav")
+            test_flag = 1
         elif volume < 0:
             volume_song = song - abs(volume)
             volume_song.export(str(wav_output_name), format="wav")
-    else:
+            test_flag = 2
+    else:   #no alteration in volume
         FluidSynth(str(soundfont_name)).midi_to_audio(midi_in_name, wav_output_name)
-    return wav_output_name
+        test_flag = 3
+    return test_flag
 
-
+"""
+find_deleteable()
+deletes all intermediary files created during the overlayWav merging of .wav files
+"""
 def find_deleteable():
+    flag = False
     files = [f for f in os.listdir('.') if os.path.isfile(f)]
     for f in files:
         if re.search("sound.+", f):
             os.remove(f)
+            flag = True
+    return flag
 
+"""
+createWav takes:
+a list of strings where each item is a name representing the soundbank being used for the conversion.
+a string name representing the name of the midi file we wish to use with the soundbank.
+a string name representing the name of the outputted .wav file we would like to save the conversion as.
+an integer (other than zero). Positive integers will make the .wav louder, while negative integers will make the .wav quieter.
+
+creates 1 .wav file from the midi file inputted using the inputted soundbanks voice
+"""
 
 def overlayWavs(soundfont_list, midi_in_name, wav_output_name, db_boost=0):
+    test_flag = 0
     number_of_overlays = len(soundfont_list)
     sounds = []
-    # empty = AudioSegment.empty()
-    createWav(midi_in_name, soundfont_list[0], "sound0.wav", db_boost)
+    try:
+        createWav(midi_in_name, soundfont_list[0], "sound0.wav", db_boost)
+    except:
+        print("an exception occured: one of the file paths/files does not exist")
+        return -1
     sound_base = AudioSegment.from_wav("sound0.wav")
     for i in range(1, number_of_overlays):
-        createWav(midi_in_name, soundfont_list[i], "sound" + str(i) + ".wav", db_boost)
+        try:
+            createWav(midi_in_name, soundfont_list[i], "sound" + str(i) + ".wav", db_boost)
+        except:
+            print("an exception occured: one of the file paths/files does not exist")
+            return -1
         sound_added = AudioSegment.from_wav("sound" + str(i) + ".wav")
         sounds.append(sound_added)
     # rename the file into the parameter passed in
@@ -58,4 +91,7 @@ def overlayWavs(soundfont_list, midi_in_name, wav_output_name, db_boost=0):
         # sound_base = sound_base.overlay(sound, gain_during_overlay=int(db_boost))
         sound_base = sound_base.overlay(sound)
     sound_base.export(wav_output_name, format="wav")
-    find_deleteable()
+    if find_deleteable() != True:
+        print("find deleteables did not delete anything!")
+        test_flag = -1
+    return test_flag
--- MidiConvert/cbo/views.py
+++ MidiConvert/cbo/views.py
@@ -90,5 +90,70 @@ def runTestingSuite(request):
 		'pass':['The testing suite has been reached.'], 
 		'fail':[]
 	}
+	# TEST SECTION 1: createWav
+	# Test 1
+	if midi_to_audio_conversion.createWav('filein.midd', '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav') = -1:
+		test_results['pass'].append("Test 1: midi input extension detection pass")
+	else:
+		test_results['fail'].append("Test 1: Error in midi input extension detection")
+
+	# Test 2
+	if midi_to_audio_conversion.createWav('filein.mid', '/app/cbo/soundfonts/Early Ens.sf3', 'melody.wav') = -1:
+		test_results['pass'].append("Test 2: soundfont input extension detection pass")
+	else:
+		test_results['fail'].append("Test 2: Error in soundfont input extension detection")
+
+	# Test 3
+	if midi_to_audio_conversion.createWav('filein.mid', '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wave') = -1:
+		test_results['pass'].append("Test 3: wav output extension detection pass")
+	else:
+		test_results['fail'].append("Test 3: Error in wav output extension detection")
+
+	# Test 4
+	if midi_to_audio_conversion.createWav('filein.mid', '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav',20) = 1:
+		test_results['pass'].append("Test 4: decibel conversion output - positive value pass")
+	else:
+		test_results['fail'].append("Test 4: Error decibel conversion output - not turning up volume")
+
+	# Test 5
+	if midi_to_audio_conversion.createWav('filein.mid', '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav',-20) = 2:
+		test_results['pass'].append("Test 5: decibel conversion output - negative value pass")
+	else:
+		test_results['fail'].append("Test 5: Error decibel conversion output - not turning down volume")
+
+	# Test 6
+	if midi_to_audio_conversion.createWav('filein.mid', '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav') = 3:
+		test_results['pass'].append("Test 6: basic conversion - no decibel alteration pass")
+	else:
+		test_results['fail'].append("Test 6: Error in basic conversion - no decibel alteration")
+
+	# TEST SECTION 2: OVERLAYWAV
+	sound_list = ['/app/cbo/soundfonts/Early Ens.sf2', '/app/cbo/soundfonts/Piano.sf2', '/app/cbo/soundfonts/Sax Section.sf2', '/app/cbo/soundfonts/Celesta.sf2']
+	bad_sound_list = ['/app/cbo/soundfonts/Early Ens.sf2', '/app/cbo/soundfonts/Piano.sf3', '/app/cbo/soundfonts/Sax Section.sf2', '/app/cbo/soundfonts/Celesta.sf2']
+	
+	# Test 7
+	if midi_to_audio_conversion.overlayWavs(sound_list, '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav') = 0:
+		test_results['pass'].append("Test 7: basic conversion - overlayWav works - no added Db")
+	else:
+		test_results['fail'].append("Test 7: Error in basic conversion - overlayWav does not work - no added db - perhaps file path error?")
+
+	# Test 8
+	if midi_to_audio_conversion.overlayWavs(sound_list, '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav', 10) = 0:
+		test_results['pass'].append("Test 7: basic conversion - overlayWav works - higher Db")
+	else:
+		test_results['fail'].append("Test 7: Error in basic conversion - overlayWav does not work - higher db - perhaps file path error?")
+
+	# Test 9
+	if midi_to_audio_conversion.overlayWavs(sound_list, '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav', -10) = 0:
+		test_results['pass'].append("Test 7: basic conversion - overlayWav works - lowered Db")
+	else:
+		test_results['fail'].append(
+			"Test 7: Error in basic conversion - overlayWav does not work - lowered db - perhaps file path error?")
+
+	# Test 10
+	if midi_to_audio_conversion.overlayWavs(bad_sound_list, '/app/cbo/soundfonts/Early Ens.sf2', 'melody.wav') = -1:
+		test_results['pass'].append("Test 7: basic conversion - overlayWav has correctly identified an error in the soundlist - no added Db")
+	else:
+		test_results['fail'].append("Test 7: Error in basic conversion - overlayWav does not recognize bad file - no added db - perhaps file path error?")
 
 	return HttpResponse(test_results, content_type='application/json')
